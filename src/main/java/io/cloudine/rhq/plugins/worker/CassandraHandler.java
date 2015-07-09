f69b3b6bbefe0b11238ea9d79b03eb7beadbb01f
Byoung Gon, Kim <fharenheit@gmail.com>
2015-07-09 18:43:19 +0900

    포맷팅.

--- src/main/java/io/cloudine/rhq/plugins/worker/CassandraHandler.java
+++ src/main/java/io/cloudine/rhq/plugins/worker/CassandraHandler.java
@@ -13,61 +13,61 @@ import java.util.Iterator;
 
 public class CassandraHandler {
 
-    protected Session session;
-    protected CassandraOperations cassandraOps;
-    protected Cluster cluster;
+	protected Session session;
+	protected CassandraOperations cassandraOps;
+	protected Cluster cluster;
 
-    public CassandraHandler openSession(String host, String port, String username, String password, String keyspace) {
-        Cluster.Builder builder = Cluster.builder().addContactPoint(host);
+	public CassandraHandler openSession(String host, String port, String username, String password, String keyspace) {
+		Cluster.Builder builder = Cluster.builder().addContactPoint(host);
 
-        if(!StringUtils.isEmpty(port)) {
-            builder.withPort(Integer.parseInt(port.trim()));
-        }
+		if (!StringUtils.isEmpty(port)) {
+			builder.withPort(Integer.parseInt(port.trim()));
+		}
 
-        if (!StringUtils.isEmpty(username) && StringUtils.isEmpty(password)) {
-            builder.withCredentials(username, password);
-        }
+		if (!StringUtils.isEmpty(username) && StringUtils.isEmpty(password)) {
+			builder.withCredentials(username, password);
+		}
 
-        cluster = builder.build();
-        session = cluster.connect(keyspace);
-        cassandraOps = new CassandraTemplate(session);
-        return this;
-    }
+		cluster = builder.build();
+		session = cluster.connect(keyspace);
+		cassandraOps = new CassandraTemplate(session);
+		return this;
+	}
 
-    public void close() {
-        session.close();
-        cluster.close();
-    }
+	public void close() {
+		session.close();
+		cluster.close();
+	}
 
-    public Iterator select(String table, String column1, String column2, Object value1, Object value2) {
-        Select select = QueryBuilder.select().all().from(table);
-        select.where(QueryBuilder.eq(column1, value1)).and(QueryBuilder.eq(column2, value2));
-        ResultSet resultSet = session.execute(select);
-        return resultSet.iterator();
-    }
+	public Iterator select(String table, String column1, String column2, Object value1, Object value2) {
+		Select select = QueryBuilder.select().all().from(table);
+		select.where(QueryBuilder.eq(column1, value1)).and(QueryBuilder.eq(column2, value2));
+		ResultSet resultSet = session.execute(select);
+		return resultSet.iterator();
+	}
 
-    public Session getSession() {
-        return session;
-    }
+	public Session getSession() {
+		return session;
+	}
 
-    public void setSession(Session session) {
-        this.session = session;
-    }
+	public void setSession(Session session) {
+		this.session = session;
+	}
 
-    public Cluster getCluster() {
-        return cluster;
-    }
+	public Cluster getCluster() {
+		return cluster;
+	}
 
-    public void setCluster(Cluster cluster) {
-        this.cluster = cluster;
-    }
+	public void setCluster(Cluster cluster) {
+		this.cluster = cluster;
+	}
 
-    public CassandraOperations getCassandraOps() {
-        return cassandraOps;
-    }
+	public CassandraOperations getCassandraOps() {
+		return cassandraOps;
+	}
 
-    public void setCassandraOps(CassandraOperations cassandraOps) {
-        this.cassandraOps = cassandraOps;
-    }
+	public void setCassandraOps(CassandraOperations cassandraOps) {
+		this.cassandraOps = cassandraOps;
+	}
 
 }
--- src/main/java/io/cloudine/rhq/plugins/worker/WorkerDiscovery.java
+++ src/main/java/io/cloudine/rhq/plugins/worker/WorkerDiscovery.java
@@ -11,42 +11,42 @@ import java.util.Set;
 
 public class WorkerDiscovery implements ResourceDiscoveryComponent, ManualAddFacet {
 
-    private static final Log LOG = LogFactory.getLog(WorkerComponent.class);
+	private static final Log LOG = LogFactory.getLog(WorkerComponent.class);
 
-    @Override
-    public Set<DiscoveredResourceDetails> discoverResources(ResourceDiscoveryContext context) {
-        LOG.info("Started Fingerprint Worker Discovery Component. (Auto)");
-        Set<DiscoveredResourceDetails> servers = new LinkedHashSet<DiscoveredResourceDetails>();
-        LOG.info("Ended Fingerprint Worker Discovery Component. (Auto)");
-        return servers;
-    }
+	@Override
+	public Set<DiscoveredResourceDetails> discoverResources(ResourceDiscoveryContext context) {
+		LOG.info("Started Fingerprint Worker Discovery Component. (Auto)");
+		Set<DiscoveredResourceDetails> servers = new LinkedHashSet<DiscoveredResourceDetails>();
+		LOG.info("Ended Fingerprint Worker Discovery Component. (Auto)");
+		return servers;
+	}
 
-    protected DiscoveredResourceDetails createResourceDetails(ResourceDiscoveryContext context, Configuration conf, ProcessInfo processInfo) throws InvalidPluginConfigurationException {
-        String host = conf.getSimpleValue("host");
-        String port = conf.getSimpleValue("port");
-        String keyspace = conf.getSimpleValue("keyspace");
-        String key = "" + (host + port + keyspace).hashCode();
-        String name = conf.getSimpleValue("name");
+	protected DiscoveredResourceDetails createResourceDetails(ResourceDiscoveryContext context, Configuration conf, ProcessInfo processInfo) throws InvalidPluginConfigurationException {
+		String host = conf.getSimpleValue("host");
+		String port = conf.getSimpleValue("port");
+		String keyspace = conf.getSimpleValue("keyspace");
+		String key = "" + (host + port + keyspace).hashCode();
+		String name = conf.getSimpleValue("name");
 
-        LOG.info("[Manual] =======================================");
-        LOG.info("key: " + key);
-        LOG.info("name: " + conf.getSimpleValue("name"));
-        LOG.info("host: " + conf.getSimpleValue("host"));
-        LOG.info("port: " + conf.getSimpleValue("port"));
-        LOG.info("username: " + conf.getSimpleValue("username"));
-        LOG.info("password: " + conf.getSimpleValue("password"));
-        LOG.info("keyspace: " + conf.getSimpleValue("keyspace"));
-        LOG.info("[Manual] =======================================");
+		LOG.info("[Manual] =======================================");
+		LOG.info("key: " + key);
+		LOG.info("name: " + conf.getSimpleValue("name"));
+		LOG.info("host: " + conf.getSimpleValue("host"));
+		LOG.info("port: " + conf.getSimpleValue("port"));
+		LOG.info("username: " + conf.getSimpleValue("username"));
+		LOG.info("password: " + conf.getSimpleValue("password"));
+		LOG.info("keyspace: " + conf.getSimpleValue("keyspace"));
+		LOG.info("[Manual] =======================================");
 
-        return new DiscoveredResourceDetails(context.getResourceType(), name, name, "0.1", "Worker", conf, processInfo);
-    }
+		return new DiscoveredResourceDetails(context.getResourceType(), name, name, "0.1", "Worker", conf, processInfo);
+	}
 
-    @Override
-    public DiscoveredResourceDetails discoverResource(Configuration conf, ResourceDiscoveryContext resourceDiscoveryContext) throws InvalidPluginConfigurationException {
-        LOG.info("Started Fingerprint Worker Discovery Component. (Manual)");
+	@Override
+	public DiscoveredResourceDetails discoverResource(Configuration conf, ResourceDiscoveryContext resourceDiscoveryContext) throws InvalidPluginConfigurationException {
+		LOG.info("Started Fingerprint Worker Discovery Component. (Manual)");
 
-        DiscoveredResourceDetails resourceDetails = createResourceDetails(resourceDiscoveryContext, conf, null);
-        LOG.info("Ended Fingerprint Worker Discovery Component. (Manual)");
-        return resourceDetails;
-    }
+		DiscoveredResourceDetails resourceDetails = createResourceDetails(resourceDiscoveryContext, conf, null);
+		LOG.info("Ended Fingerprint Worker Discovery Component. (Manual)");
+		return resourceDetails;
+	}
 }
