de16a1813e1d99e0a629b64a256c01f78ee3f734
Byoung Gon, KIM <fharenheit@gmail.com>
2015-07-15 22:06:11 +0900

    메모리 모니터링 추가.

--- src/main/java/io/cloudine/rhq/plugins/worker/WorkerComponent.java
+++ src/main/java/io/cloudine/rhq/plugins/worker/WorkerComponent.java
@@ -6,6 +6,8 @@ import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.hyperic.sigar.ProcCpu;
+import org.hyperic.sigar.ProcMem;
 import org.rhq.core.domain.configuration.Configuration;
 import org.rhq.core.domain.measurement.AvailabilityType;
 import org.rhq.core.domain.measurement.MeasurementDataNumeric;
@@ -16,100 +18,114 @@ import org.rhq.core.pluginapi.inventory.ResourceComponent;
 import org.rhq.core.pluginapi.inventory.ResourceContext;
 import org.rhq.core.pluginapi.measurement.MeasurementFacet;
 import org.rhq.core.pluginapi.plugin.PluginContext;
+import org.rhq.core.system.ProcessInfo;
 
 import java.util.Set;
 
 public class WorkerComponent implements ResourceComponent, MeasurementFacet {
 
-	private static final Log LOG = LogFactory.getLog(WorkerComponent.class);
+    private static final Log LOG = LogFactory.getLog(WorkerComponent.class);
 
-	private String name;
-	private String host;
-	private String username;
-	private String password;
-	private String keyspace;
-	private String port;
+    private String name;
+    private String host;
+    private String username;
+    private String password;
+    private String keyspace;
+    private String port;
+    private String pid;
 
-	private CassandraHandler handler;
+    private CassandraHandler handler;
 
-	public void initialize(PluginContext context) throws Exception {
-		LOG.info("Initialized Worker.");
-	}
+    public void initialize(PluginContext context) throws Exception {
+        LOG.info("Initialized Worker.");
+    }
 
-	public void shutdown() {
-		LOG.info("Ended Worker.");
-	}
+    public void shutdown() {
+        LOG.info("Ended Worker.");
+    }
 
-	public AvailabilityType getAvailability() {
-		return AvailabilityType.UP;
-	}
+    public AvailabilityType getAvailability() {
+        return AvailabilityType.UP;
+    }
 
-	public void start(ResourceContext context) throws InvalidPluginConfigurationException, Exception {
-		LOG.info("Started Worker.");
+    public void start(ResourceContext context) throws InvalidPluginConfigurationException, Exception {
+        LOG.info("Started Worker.");
 
-		Configuration conf = context.getPluginConfiguration();
+        Configuration conf = context.getPluginConfiguration();
 
-		this.name = conf.getSimpleValue("name");
-		this.host = conf.getSimpleValue("host");
-		this.port = conf.getSimpleValue("port");
-		this.username = conf.getSimpleValue("username");
-		this.password = conf.getSimpleValue("password");
-		this.keyspace = conf.getSimpleValue("keyspace");
+        this.pid = conf.getSimpleValue("pid");
+        this.name = conf.getSimpleValue("name");
+        this.host = conf.getSimpleValue("host");
+        this.port = conf.getSimpleValue("port");
+        this.username = conf.getSimpleValue("username");
+        this.password = conf.getSimpleValue("password");
+        this.keyspace = conf.getSimpleValue("keyspace");
 
-		LOG.info("[Component] =======================================");
-		LOG.info("name: " + name);
-		LOG.info("host: " + host);
-		LOG.info("port: " + port);
-		LOG.info("username: " + username);
-		LOG.info("password: " + password);
-		LOG.info("keyspace: " + keyspace);
-		LOG.info("[Component] =======================================");
-	}
+        LOG.info("[Component] =======================================");
+        LOG.info("name: " + name);
+        LOG.info("host: " + host);
+        LOG.info("port: " + port);
+        LOG.info("username: " + username);
+        LOG.info("password: " + password);
+        LOG.info("keyspace: " + keyspace);
+        LOG.info("[Component] =======================================");
+    }
 
-	public void stop() {
-		LOG.info("Stopped Worker.");
-	}
+    public void stop() {
+        LOG.info("Stopped Worker.");
+    }
 
-	public void getValues(MeasurementReport report, Set<MeasurementScheduleRequest> metrics) throws Exception {
-		LOG.info("[Values] ---------------------------------------");
-		LOG.info("name: " + name);
-		LOG.info("host: " + host);
-		LOG.info("port: " + port);
-		LOG.info("username: " + username);
-		LOG.info("password: " + password);
-		LOG.info("keyspace: " + keyspace);
-		LOG.info("[Values] ---------------------------------------");
+    public void getValues(MeasurementReport report, Set<MeasurementScheduleRequest> metrics) throws Exception {
+        LOG.info("[Values] ---------------------------------------");
+        LOG.info("name: " + name);
+        LOG.info("host: " + host);
+        LOG.info("port: " + port);
+        LOG.info("username: " + username);
+        LOG.info("password: " + password);
+        LOG.info("keyspace: " + keyspace);
+        LOG.info("pid: " + pid);
+        LOG.info("[Values] ---------------------------------------");
 
-		CassandraHandler handler = new CassandraHandler();
-		handler.openSession(host, port, username, password, keyspace);
+        CassandraHandler handler = new CassandraHandler();
+        handler.openSession(host, port, username, password, keyspace);
 
-		for (MeasurementScheduleRequest request : metrics) {
-			if ("playtime".equals(request.getName())) {
-				String sql = "SELECT size FROM audio_total_fingerprint WHERE id = 1";
+        for (MeasurementScheduleRequest request : metrics) {
+            if ("playtime".equals(request.getName())) {
+                String sql = "SELECT size FROM audio_total_fingerprint WHERE id = 1";
 
-				PreparedStatement statement = handler.getSession().prepare(sql);
-				ResultSet results = handler.getSession().execute(statement.bind());
-				Row row = results.one();
+                PreparedStatement statement = handler.getSession().prepare(sql);
+                ResultSet results = handler.getSession().execute(statement.bind());
+                Row row = results.one();
 
-				int expected = row.getInt(0);
-				float millis = (expected * 2048 * 1000) / (16 * 11025);
+                int expected = row.getInt(0);
+                float millis = (expected * 2048 * 1000) / (16 * 11025);
 
-				report.addData(new MeasurementDataNumeric(request, Double.parseDouble(String.valueOf(millis))));
-			} else if ("count".equals(request.getName())) {
-				String sql = "SELECT COUNT(*) FROM AUDIO_FILE_LIST";
+                report.addData(new MeasurementDataNumeric(request, Double.parseDouble(String.valueOf(millis))));
+            } else if ("count".equals(request.getName())) {
+                String sql = "SELECT COUNT(*) FROM AUDIO_FILE_LIST";
 
-				PreparedStatement statement = handler.getSession().prepare(sql);
-				ResultSet results = handler.getSession().execute(statement.bind());
-				Row row = results.one();
-				long expected = row.getLong(0);
+                PreparedStatement statement = handler.getSession().prepare(sql);
+                ResultSet results = handler.getSession().execute(statement.bind());
+                Row row = results.one();
+                long expected = row.getLong(0);
 
-				report.addData(new MeasurementDataNumeric(request, Double.parseDouble("" + expected)));
-			}
-		}
+                report.addData(new MeasurementDataNumeric(request, Double.parseDouble("" + expected)));
+            } else if ("cpu".equals(request.getName())) {
+                ProcessInfo processInfo = new ProcessInfo(Long.parseLong(pid));
+                ProcCpu cpu = processInfo.priorSnaphot().getCpu();
 
-		try {
-			handler.close();
-		} catch (Exception e) {
-		}
-	}
+                report.addData(new MeasurementDataNumeric(request, Double.parseDouble("" + cpu.getTotal())));
+            } else if ("ram".equals(request.getName())) {
+                ProcessInfo processInfo = new ProcessInfo(Long.parseLong(pid));
+                ProcMem memory = processInfo.priorSnaphot().getMemory();
+
+                report.addData(new MeasurementDataNumeric(request, Double.parseDouble("" + memory.getSize())));
+            }
+        }
+
+        try {
+            handler.close();
+        } catch (Exception e) {
+        }
+    }
 }
--- src/main/java/io/cloudine/rhq/plugins/worker/WorkerDiscovery.java
+++ src/main/java/io/cloudine/rhq/plugins/worker/WorkerDiscovery.java
@@ -3,50 +3,86 @@ package io.cloudine.rhq.plugins.worker;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.rhq.core.domain.configuration.Configuration;
+import org.rhq.core.domain.configuration.Property;
 import org.rhq.core.pluginapi.inventory.*;
 import org.rhq.core.system.ProcessInfo;
+import org.springframework.core.io.FileSystemResource;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.LinkedHashSet;
+import java.util.Properties;
 import java.util.Set;
 
 public class WorkerDiscovery implements ResourceDiscoveryComponent, ManualAddFacet {
 
-	private static final Log LOG = LogFactory.getLog(WorkerComponent.class);
+    private static final Log LOG = LogFactory.getLog(WorkerComponent.class);
 
-	@Override
-	public Set<DiscoveredResourceDetails> discoverResources(ResourceDiscoveryContext context) {
-		LOG.info("Started Fingerprint Worker Discovery Component. (Auto)");
-		Set<DiscoveredResourceDetails> servers = new LinkedHashSet<DiscoveredResourceDetails>();
-		LOG.info("Ended Fingerprint Worker Discovery Component. (Auto)");
-		return servers;
-	}
+    @Override
+    public Set<DiscoveredResourceDetails> discoverResources(ResourceDiscoveryContext context) {
+        LOG.info("Started Fingerprint Worker Discovery Component. (Auto)");
+        Set<DiscoveredResourceDetails> servers = new LinkedHashSet<DiscoveredResourceDetails>();
+        LOG.info("Ended Fingerprint Worker Discovery Component. (Auto)");
+        return servers;
+    }
 
-	protected DiscoveredResourceDetails createResourceDetails(ResourceDiscoveryContext context, Configuration conf, ProcessInfo processInfo) throws InvalidPluginConfigurationException {
-		String host = conf.getSimpleValue("host");
-		String port = conf.getSimpleValue("port");
-		String keyspace = conf.getSimpleValue("keyspace");
-		String key = "" + (host + port + keyspace).hashCode();
-		String name = conf.getSimpleValue("name");
+    protected DiscoveredResourceDetails createResourceDetails(ResourceDiscoveryContext context, Configuration conf, ProcessInfo processInfo) throws InvalidPluginConfigurationException {
+        String pidfile = conf.getSimpleValue("pidfile");
+        String propertiesfile = conf.getSimpleValue("propertiesfile");
 
-		LOG.info("[Manual] =======================================");
-		LOG.info("key: " + key);
-		LOG.info("name: " + conf.getSimpleValue("name"));
-		LOG.info("host: " + conf.getSimpleValue("host"));
-		LOG.info("port: " + conf.getSimpleValue("port"));
-		LOG.info("username: " + conf.getSimpleValue("username"));
-		LOG.info("password: " + conf.getSimpleValue("password"));
-		LOG.info("keyspace: " + conf.getSimpleValue("keyspace"));
-		LOG.info("[Manual] =======================================");
+        FileSystemResource resource = new FileSystemResource(pidfile);
+        long pid = 0;
+        try {
+            String pidString = ResourceUtils.getResourceTextContents(resource);
+            pid = Long.parseLong(pidString);
+        } catch (IOException e) {
+            throw new InvalidPluginConfigurationException("Unable to load process id file", e);
+        }
 
-		return new DiscoveredResourceDetails(context.getResourceType(), name, name, "0.1", "Worker", conf, processInfo);
-	}
+        Properties props = new Properties();
+        try {
+            File propertiesFile = new File(propertiesfile);
+            props.load(new FileInputStream(propertiesFile));
+        } catch (Exception e) {
+            throw new InvalidPluginConfigurationException("Unable to load process properties file", e);
+        }
 
-	@Override
-	public DiscoveredResourceDetails discoverResource(Configuration conf, ResourceDiscoveryContext resourceDiscoveryContext) throws InvalidPluginConfigurationException {
-		LOG.info("Started Fingerprint Worker Discovery Component. (Manual)");
+        String name = conf.getSimpleValue("name");
+        String host = props.getProperty(name + "." + "host");
+        String port = props.getProperty(name + "." + "port");
+        String keyspace = props.getProperty(name + "." + "keyspace");
+        String username = props.getProperty(name + "." + "username");
+        String password = props.getProperty(name + "." + "password");
+        String key = "" + (host + port + keyspace).hashCode();
 
-		DiscoveredResourceDetails resourceDetails = createResourceDetails(resourceDiscoveryContext, conf, null);
-		LOG.info("Ended Fingerprint Worker Discovery Component. (Manual)");
-		return resourceDetails;
-	}
+        context.getDefaultPluginConfiguration().setSimpleValue("key", key);
+        context.getDefaultPluginConfiguration().setSimpleValue("pid", String.valueOf(pid));
+        context.getDefaultPluginConfiguration().setSimpleValue("host", host);
+        context.getDefaultPluginConfiguration().setSimpleValue("port", port);
+        context.getDefaultPluginConfiguration().setSimpleValue("keyspace", keyspace);
+        context.getDefaultPluginConfiguration().setSimpleValue("username", username);
+        context.getDefaultPluginConfiguration().setSimpleValue("password", password);
+
+        LOG.info("[Manual] =======================================");
+        LOG.info("key: " + key);
+        LOG.info("name: " + conf.getSimpleValue("name"));
+        LOG.info("host: " + host);
+        LOG.info("port: " + port);
+        LOG.info("username: " + username);
+        LOG.info("password: " + password);
+        LOG.info("keyspace: " + keyspace);
+        LOG.info("[Manual] =======================================");
+
+        return new DiscoveredResourceDetails(context.getResourceType(), name, name, "0.1", "Worker", conf, new ProcessInfo(pid));
+    }
+
+    @Override
+    public DiscoveredResourceDetails discoverResource(Configuration conf, ResourceDiscoveryContext resourceDiscoveryContext) throws InvalidPluginConfigurationException {
+        LOG.info("Started Fingerprint Worker Discovery Component. (Manual)");
+
+        DiscoveredResourceDetails resourceDetails = createResourceDetails(resourceDiscoveryContext, conf, null);
+        LOG.info("Ended Fingerprint Worker Discovery Component. (Manual)");
+        return resourceDetails;
+    }
 }
